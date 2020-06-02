7a2186c72f0d09aba23687aa7dbccefec8c31c7c
GMcLean <g.mclean@uq.edu.au>
2020-06-03 08:09:42 +1000

    Add project files.

---
+++ SVPfractForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SVPfract
+{
+    partial class SPVfractForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MetFileLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.MetFileNameLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate SVPfract using hourly Temperature and Humidity";
+            // 
+            // MetFileLinkLabel
+            // 
+            this.MetFileLinkLabel.AutoSize = true;
+            this.MetFileLinkLabel.Location = new System.Drawing.Point(26, 55);
+            this.MetFileLinkLabel.Name = "MetFileLinkLabel";
+            this.MetFileLinkLabel.Size = new System.Drawing.Size(137, 16);
+            this.MetFileLinkLabel.TabIndex = 1;
+            this.MetFileLinkLabel.TabStop = true;
+            this.MetFileLinkLabel.Text = "Select Hourly Met File";
+            this.MetFileLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.MetFileLinkLabel_LinkClicked);
+            // 
+            // MetFileNameLabel
+            // 
+            this.MetFileNameLabel.AutoSize = true;
+            this.MetFileNameLabel.Location = new System.Drawing.Point(192, 55);
+            this.MetFileNameLabel.Name = "MetFileNameLabel";
+            this.MetFileNameLabel.Size = new System.Drawing.Size(61, 16);
+            this.MetFileNameLabel.TabIndex = 2;
+            this.MetFileNameLabel.Text = "Filename";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "\"C:\\Pioneer\\SPVfract\\WoodlandHourly2019.csv\"";
+            this.openFileDialog.Filter = "Met File (*.csv)|*.csv";
+            // 
+            // SPVfractForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 789);
+            this.Controls.Add(this.MetFileNameLabel);
+            this.Controls.Add(this.MetFileLinkLabel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "SPVfractForm";
+            this.Text = "SVPfract";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel MetFileLinkLabel;
+        private System.Windows.Forms.Label MetFileNameLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ SVPfractForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SVPfract
+{
+    public partial class SPVfractForm : Form
+    {
+        public SPVfractForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MetFileLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Get the met file data
+            MetFileNameLabel.Text = @"C:\Pioneer\SPVfract\WoodlandHourly2019.csv";
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            MetFileNameLabel.Text = openFileDialog.FileName;
+            ReadMetData();
+        }
+        private void ReadMetData()
+        {
+
+        }
+    }
+}
